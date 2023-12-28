ff1f3935d1231ce0c4a4236f7faffb3ec0b01758
HallisCode <applgoolhal@gmail.com>
2023-12-28 17:41:59 +0300

    Add Infrastructure layer

---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities.ArticleScope;
+using Domain.Entities.UserScope;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public DbSet<Article> Article { get; set; }
+		public DbSet<Review> Review { get; set; }
+		public DbSet<ReviewComment> ReviewComment { get; set; }
+		public DbSet<Tag> Tag { get; set; }
+		public DbSet<User> User { get; set; }
+		public DbSet<Security> Security { get; set; }
+
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+	}
+}
