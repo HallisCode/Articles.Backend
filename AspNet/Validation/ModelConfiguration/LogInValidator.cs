71cd819025c9141eea0748163e1ad7e2231610e7
HallisCode <applgoolhal@gmail.com>
2024-01-19 23:14:13 +0300

    Настройки валидации

---
+++ AspNet/Validation/ModelConfiguration/LogInValidator.cs
@@ -0,0 +1,9 @@
+﻿using AspNet.Dto.Request;
+using FluentValidation;
+
+namespace AspNet.Validation.ModelConfiguration
+{
+	public class LogInValidator : AbstractValidator<LogInDto>
+	{
+	}
+}
---
+++ AspNet/Validation/ModelConfiguration/RegistryValidation.cs
@@ -0,0 +1,17 @@
+﻿using AspNet.Dto.Request;
+using FluentValidation;
+
+namespace AspNet.Validation.ModelConfiguration 
+{
+	public class RegistryValidation : AbstractValidator<RegistryDto>
+	{
+		public RegistryValidation()
+		{
+			RuleFor(registryDto => registryDto.Nickname).NotEmpty().MinimumLength(6).MaximumLength(32);
+
+			RuleFor(registryDto => registryDto.Email).NotEmpty().MaximumLength(256).EmailAddress();
+
+			RuleFor(registryDto => registryDto.Password).NotEmpty().MinimumLength(8).MaximumLength(64);
+		}
+	}
+}
