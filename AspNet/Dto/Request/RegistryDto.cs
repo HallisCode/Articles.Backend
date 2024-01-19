8c8c391ed05798e38835a039021e424b54977a9a
HallisCode <applgoolhal@gmail.com>
2024-01-19 23:13:55 +0300

    Модели входных данных

---
+++ AspNet/Dto/Request/RegistryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNet.Dto.Request
+{
+	public class RegistryDto
+	{
+		public string Nickname { get; set; }
+
+		public string Email { get; set; }
+
+		public string Password { get; set; }
+
+		public string? Bio { get; set; }
+	}
+}
