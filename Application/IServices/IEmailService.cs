ba57817d72b6e501404e1f017c17242fdf674e2d
HallisCode <applgoolhal@gmail.com>
2024-01-22 20:31:56 +0300

    Фигачим сервисы ради прикола

---
+++ Application/IServices/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Application.IServices
+{
+	public interface IEmailService
+	{
+		public Task SendMailAsync(string from, string to, string title, string content);
+	}
+}
---
+++ Application/IServices/ISecurityService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Application.IServices
+{
+	/// <summary>
+	/// <para>Интерфейс для смены почты в 3 этапа :</para>
+	/// <para>1. Создать токен для подтверждения смены почты для текущей почты.</para>
+	/// <para>2. Создать токен для потверждения смены почты для новой почты, на основе предыдущего токена.</para>
+	/// <para>3. Подтвердить смену почты на основе токена из пункта 2.</para>
+	/// </summary>
+	public interface ISecurityService
+	{
+		/// <summary>
+		/// Меняем пароль со старого на новый.
+		/// </summary>
+		/// <returns></returns>
+		public Task ChangePasswordAsync(long userId, string oldPassword, string newPassword);
+
+		/// <summary>
+		/// Создаём токен для подтверждения смены почты, на старой почте.
+		/// </summary>
+		/// <returns></returns>
+		public Task<string> CreateTokenChangeEmailOnOldAsync(long userId, string oldEmail, string newEmail);
+
+		/// <summary>
+		/// Создаём токен для подтверждения почты, на новой почте.
+		/// </summary>
+		/// <returns></returns>
+		public Task<string> CreateTokenChangeEmailOnNewAsync(string jwtTokenData);
+
+		/// <summary>
+		/// Подтверждаем токен на смену почты, полученный на новую почту.
+		/// </summary>
+		/// <returns></returns>
+		public Task ConfirmEmailChangeAsync(string jwtTokenData);
+	}
+}
