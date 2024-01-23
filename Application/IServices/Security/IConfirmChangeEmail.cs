9d3708a6d61f74ca771437e1beaa7d26f6d799e9
HallisCode <applgoolhal@gmail.com>
2024-01-23 23:09:14 +0300

    Refactoring

--- Application/IServices/Security/IConfirmChangeEmail.cs
+++ Application/IServices/Security/IConfirmChangeEmail.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.IServices.Security
+﻿namespace Application.IServices.Security
 {
 	public interface IConfirmChangeEmail<TRequest> : IConfirmChangeByOldEmail<TRequest>, IConfirmChangeByNewEmail<TRequest>
 	{
