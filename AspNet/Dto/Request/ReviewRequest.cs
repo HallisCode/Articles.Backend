bca30f2cac1489fe600ebed8939c8af405c76f7d
HallisCode <applgoolhal@gmail.com>
2024-02-03 19:50:16 +0300

    not important

---
+++ AspNet/Dto/Request/ReviewRequest.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enum;
+
+namespace AspNet.Dto.Request
+{
+	public class ReviewRequest
+	{
+		public long ArticleId { get; set; }
+
+		public string Content { get; set; }
+
+		public ReviewType Type { get; set; }
+	}
+}
--- AspNet/Dto/Response/ReviewResponse.cs
+++ AspNet/Dto/Response/ReviewResponse.cs
@@ -1,10 +1,13 @@
-﻿using Domain.Enum;
+﻿using Domain.Entities.UserScope;
+using Domain.Enum;
 
 namespace AspNet.Dto.Response
 {
 	public class ReviewResponse
 	{
 		public long Id { get; set; }
+
+		public User Author { get; set; }
 
 		public string Content { get; set; }
 
